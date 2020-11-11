8dc62039f06bd37524f97d7f64cc65771c91ed10
Grini Youssef <grini.youssef@gmail.com>
2020-11-11 21:42:53 +0100

    Set up for deployment

--- src/app/servides/product.service.ts
+++ src/app/servides/product.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { Product } from '../common/product';
 import { map } from 'rxjs/operators';
 import { ProductCategory } from '../common/product-category';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,8 @@ import { ProductCategory } from '../common/product-category';
 
 export class ProductService {
 
-  private baseUrl = 'http://localhost:8080/api/products';
-  private categoryUrl = "http://localhost:8080/api/product-category";
+  private baseUrl = environment.baseUrl + 'api/products';
+  private categoryUrl = environment.baseUrl +  'api/product-category';
 
   constructor( private httpClient: HttpClient ) { 
 
